2634721e7232fcd3e7abb96c123d3619e76abc71
jolahmilovski@gmail.com <jolahmilovski@gmail.com>
2024-09-09 00:30:10 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace SecondArrayExp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] friendName = { "First Friend", "Second Friend", "Third Friend" };
+            
+            Console.WriteLine($"Here are {friendName.Length} of my friends: \n");
+            //for ( int i = 0; i < friendName.Length; i++)
+            //{
+            //    Console.WriteLine( friendName[i] );
+            //}
+            Array.Resize(ref friendName, 6);
+            friendName[3] = "4 friend";
+            friendName[4] = "5 friend";
+            friendName[5] = "6 friend";
+            foreach (string friend in friendName)
+            {
+                Console.WriteLine(friend);
+            }
+            Console.ReadLine();
+        }
+    }
+}
